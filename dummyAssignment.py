c37376e6fa4ba12e6cf3a4439cd882661f3e3cc3
tsagaanb <tsagaanb@bu.edu>
2024-09-13 19:26:31 -0400

    Adding the python code for the Dummy Assignment - 0

---
+++ dummyAssignment.py
@@ -0,0 +1,12 @@
+# Tsagaandari Battugs
+# CAS CS506 - Boston University
+# Dummy assignment - 0
+
+def sum(a,b):
+    """
+    # A python function that adds two numbers 
+    # together and prints their sum to the 
+    # command line
+    """
+
+    print(a+b)
